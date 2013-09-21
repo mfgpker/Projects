6753c21f56775e2a5567897771447d1edff460ac
mfgpker <ipodlikeme@gmail.com>
2013-09-21 23:12:46 +0200

    update to Factor

--- Numbers/Factor.py
+++ Numbers/Factor.py
@@ -1,15 +1,33 @@
 print "Factor Loaded"
+def factorial(num):
+	"""
+    Prints factorial for a given number
+    Recursive solution
+    """
+	if num == 0:
+		return 1
+	else:
+		return num * factorial(num - 1)
+print ""
+print "factorial: "
+print factorial(1)
+print factorial(5)
+print factorial(10)
 
-def factor(x):
-	i = 0
-	f = 1
-	while i <= x:
-		i+=1
-		f*=i
-	return f
 
-def main():
-	x = int (raw_input("Please input the first integer: "))
-	print (str(factor(x)))
-
-main()
+def factorial2(num):
+	"""
+    Prints factorial for a given number
+    Loop solution
+    """
+	factorial = 1
+	if num == 0:
+		return 1
+	for i in range(1, num+1):
+		factorial *= i
+	return factorial
+print ""
+print "factorial2: "
+print factorial2(1)
+print factorial2(5)
+print factorial2(10)
